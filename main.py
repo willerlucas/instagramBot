c3ddf23e7ccab24c4200528b2bead6f3e42561da
Willer Lucas Barata Castanheti <willerlbc@gmail.com>
2020-03-24 16:21:11 -0300

    adicionada função scroll() e melhorada função Wait()

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-# necessário ter o geckodriver instalado e adicionado ao Path em variaveis de ambientes (para firefox)
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.webdriver.common.keys import Keys
 from time import sleep
 import random
 
 
 class InstagramBot:
+
+    # Funcao LOGIN INICIAL
     def __init__(self, username, password):
-        # Login
+        # Abrindo Browser
         print("abrindo firefox")
         self.driver = webdriver.Firefox()
+
+        # Abrindo Instagram
         print("abrindo site")
         self.driver.get("https://instagram.com")
+
+        # Relizando Login
         print("Realizando login")
         sleep(5)
         self.driver.find_element_by_xpath(
@@ -20,45 +26,58 @@ class InstagramBot:
         self.driver.find_element_by_xpath(
             "//input[@name=\"password\"]").send_keys(password)
         self.driver.find_element_by_xpath('//button[@type="submit"]').click()
-        sleep(5)
-        # Close Pop-Up
+        sleep(10)
+
+        # Fechando Pop-Up
+        print("Fechando Pop-Up")
         self.driver.find_element_by_xpath(
             "//button[contains(text(), 'Agora não')]").click()
+
+    # ABRIR PAGINA DA TAG
 
     def TagSearch(self, tag):
         # abre o explorador na tag desejada
         try:
             print("Realizando pesquisa por "+str(tag))
             self.driver.get("https://www.instagram.com/explore/tags/"+tag+"/")
-            MyBot.Wait()
+            sleep(random.randint(0, 5))
         except:
             print("erro ao procurar pela tag")
 
-    def Like(self, posts):
-        # sortear uma foto para abrir
-        linha = random.randint(1,3)
-        coluna = random.randint(1,3)
-        print("abrindo a foto: ",linha,coluna)
-        print("Abrindo foto")
-        self.driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[1]/div/div/div["+str(linha)+"]/div["+str(coluna)+"]").click()
+    # REALIZAR CURTIDAS
 
-        #MyBot.Wait()
-        sleep(random.random()*10)
-        # like
+    def Like(self, posts):
+
+        # usar alguns scrolls
+        MyBot.ScrollSimulation()
+
+        # sortear uma foto para abrir
+        linha = random.randint(1, 3)
+        coluna = random.randint(1, 3)
+
+        # ABRINDO FOTO SORTEADA
+        print("abrindo a foto: ", linha, coluna)
+        print("Abrindo foto")
+
+        self.driver.find_element_by_xpath(
+            "/html/body/div[1]/section/main/article/div[1]/div/div/div["+str(linha)+"]/div["+str(coluna)+"]").click()
+        MyBot.Wait()
+
+        # LOOP PARA PASSAR FOTOS E CURTIR
         for x in range(posts):
-         
 
             try:
-            	#botão (coração) do post
+                # botão (coração) do post
                 like = self.driver.find_element_by_xpath(
                     "/html/body/div[4]/div[2]/div/article/div[2]/section[1]/span[1]/button").click()
                 MyBot.Wait()
-                #botão seguir
+
+                # botão seguir
                 #follow = self.driver.find_element_by_xpath("/html/body/div[4]/div[2]/div/article/header/div[2]/div[1]/div[2]/button").click()
                 # MyBot.Wait()
 
-                #pular quantidade aletória de postagens para evitar bloqueio por spam
-                passNumb = MyBot.RandomNext()
+                # pular quantidade aletória de postagens para evitar bloqueio por spam
+                passNumb = MyBot.RandomNum()
                 print("passei")
                 for x in range(passNumb):
                     print("passando "+str(passNumb))
@@ -66,27 +85,48 @@ class InstagramBot:
                         "//a[text()=\"Próximo\"]").click()
                     MyBot.Wait()
 
+            #gambiarra pois as vezes a pagina demora a carregar
             except:
                 self.driver.find_element_by_xpath(
                     "//a[text()=\"Próximo\"]").click()
 
-    #funcao para aguardar intervalo aletório entre uma ação e outra para evitar bloqueio por spam
+
+    # funcao para aguardar intervalo aletório entre uma ação e outra para evitar bloqueio por spam
     def Wait(self):
-        time = random.random()*50
+        time = random.random()*21
         print("Esperando "+str(time) + " segundos")
         sleep(time)
 
-    #função criada para retornar numero aleatório de posts a serem pulados
-    def RandomNext(self):
-        numb = random.randint(1, 5)
+    # função criada para retornar numero aleatório de posts a serem pulados
+    def RandomNum(self):
+        numb = random.randint(1, 3)
         return numb
 
-#INPUTS
-#INPUTS
-#INPUTS
-#INPUTS
+    def ScrollUp(self):
+        num = MyBot.RandomNum()
+        for x in range(num):
+            self.driver.find_element_by_xpath(
+                "/html/body").send_keys(Keys.PAGE_UP)
+            sleep(2)
+
+    def ScrollDown(self):
+        num = MyBot.RandomNum()
+        for x in range(num):
+            self.driver.find_element_by_xpath(
+                "/html/body").send_keys(Keys.PAGE_DOWN)
+            sleep(1)
+
+    def ScrollSimulation(self):
+        MyBot.ScrollDown()
+        MyBot.ScrollUp()
+        sleep(3)
+
+
+############################################################################################################3
+
 
 username = input("Username: ")
+
 password = input("Password: ")
 
 qntTags = input("Quantidade de tags: ")
@@ -95,7 +135,8 @@ while True:
         qntTags = int(qntTags)
         break
     except:
-        qntTags = input("A Quantidade de tags deve ser um valor númerico inteiro: ")
+        qntTags = input(
+            "A Quantidade de tags deve ser um valor númerico inteiro: ")
 
 
 qntPosts = input("Quantidade de likes por tag: ")
@@ -103,14 +144,16 @@ while True:
     try:
         qntPosts = int(qntPosts)
         break
-
     except:
-        qntPosts = input("A Quantidade de posts deve ser um valor númerico inteiro: ")	
+        qntPosts = input(
+            "A Quantidade de posts deve ser um valor númerico inteiro: ")
+
 
 tagList = []
 for x in range(qntTags):
     tag = input("Insira a tag "+str(x+1)+": ")
     tagList.append(tag)
+
 
 MyBot = InstagramBot(username, password)
 for x in tagList:
